d7dca847198783d8cca190077ab2be0e2d4ebc44
Craig Morten <124147726+jlp-craigmorten@users.noreply.github.com>
2024-01-26 10:47:08 +0000

    docs: add Vue example (#45)

---
+++ examples/vue/src/IncrementCounter.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+export default {
+  data: () => ({
+    count: 0,
+  }),
+
+  methods: {
+    increment() {
+      this.count++
+    },
+  },
+}
+</script>
+
+<template>
+  <h1>Increment Counter</h1>
+  <p aria-live="polite">Times clicked: {{ count }}</p>
+  <button @click="increment">increment</button>
+</template>
