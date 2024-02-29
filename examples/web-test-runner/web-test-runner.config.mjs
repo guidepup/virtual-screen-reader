8dfa5107ad599ab94f2ee48e15723b740c82e2ea
jlp-craigmorten <craig.morten@waitrose.co.uk>
2024-02-29 17:50:44 +0000

    refactor: remove further dependencies via upgrade and cjs plugin

--- examples/web-test-runner/web-test-runner.config.mjs
+++ examples/web-test-runner/web-test-runner.config.mjs
@@ -1,40 +1,29 @@
-import path from "path";
-import { fileURLToPath } from "url";
-import _alias from "@rollup/plugin-alias";
 import _commonjs from "@rollup/plugin-commonjs";
 import _replace from "@rollup/plugin-replace";
 import { fromRollup } from "@web/dev-server-rollup";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const alias = fromRollup(_alias);
 const commonjs = fromRollup(_commonjs);
 const replace = fromRollup(_replace);
 
 export default {
   nodeResolve: true,
   plugins: [
-    alias({
-      entries: [
-        {
-          find: "aria-query",
-          replacement: path.resolve(
-            __dirname,
-            "node_modules/aria-query-esm/lib/esm/index.js"
-          ),
-        },
-      ],
-    }),
+    // Support @testing-library/dom usage of module that checks if running in
+    // production or development.
+    // See `node_modules/@testing-library/dom/node_modules/react-is/cjs/react-is.development.js`
     replace({
       "process.env.NODE_ENV": JSON.stringify("production"),
       preventAssignment: true,
     }),
+    // Handle dependencies that are commonjs only and need compiling to support
+    // an ESM environment.
     commonjs({
       include: [
         "**/node_modules/@testing-library/**",
-        "**/node_modules/lz-string/**",
         "**/node_modules/ansi-regex/**",
+        "**/node_modules/aria-query/**",
+        "**/node_modules/dequal/**",
+        "**/node_modules/lz-string/**",
       ],
     }),
   ],
