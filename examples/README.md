3c5c4d0cf2212a49cf87bbd78b606a009aa94cf7
jlp-craigmorten <craig.morten@waitrose.co.uk>
2024-02-29 23:47:10 +0000

    docs: add README for the web-test-runner example

--- examples/README.md
+++ examples/README.md
@@ -4,6 +4,7 @@ This directory contains a series of self-contained examples that you can use as
 starting points for your setup, or as snippets to pull into your existing
 projects:
 
-| Example      | Description                                                                            |
-| ------------ | -------------------------------------------------------------------------------------- |
-| [Vue](./vue) | A basic example combining Vue, Vitest, Vue Testing Library, and Virtual Screen Reader. |
+| Example                              | Description                                                                                                               |
+| ------------------------------------ | ------------------------------------------------------------------------------------------------------------------------- |
+| [Vue](./vue)                         | A basic example combining Vue, Vitest, Vue Testing Library, and Virtual Screen Reader.                                    |
+| [Web Test Runner](./web-test-runner) | A basic example combining [Web Test Runner](https://modern-web.dev/docs/test-runner/overview/) and Virtual Screen Reader. |
--- examples/web-test-runner/web-test-runner.config.mjs
+++ examples/web-test-runner/web-test-runner.config.mjs
@@ -6,24 +6,27 @@ const commonjs = fromRollup(_commonjs);
 const replace = fromRollup(_replace);
 
 export default {
+  // Required to resolve node builtins.
   nodeResolve: true,
   plugins: [
-    // Support @testing-library/dom usage of module that checks if running in
-    // production or development.
+    // Support `@testing-library/dom` usage of a `react-is` module that checks if
+    // running in a production or development Node environment.
     // See `node_modules/@testing-library/dom/node_modules/react-is/cjs/react-is.development.js`
     replace({
       "process.env.NODE_ENV": JSON.stringify("production"),
       preventAssignment: true,
     }),
     // Handle dependencies that are commonjs only and need compiling to support
-    // an ESM environment.
+    // an ESM only context.
     commonjs({
       include: [
+        // `@testing-library/dom` and it's dependencies.
         "**/node_modules/@testing-library/**",
         "**/node_modules/ansi-regex/**",
+        "**/node_modules/lz-string/**",
+        // `aria-query` and it's dependencies.
         "**/node_modules/aria-query/**",
         "**/node_modules/dequal/**",
-        "**/node_modules/lz-string/**",
       ],
     }),
   ],
