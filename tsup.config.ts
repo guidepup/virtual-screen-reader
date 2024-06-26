3fb676198c50e7569a51fd43a44217d3926983b6
jlp-craigmorten <craig.morten@waitrose.co.uk>
2024-06-26 21:45:18 +0100

    feat: build tweaks

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,63 +1,61 @@
-import { defineConfig } from "tsup";
+import { defineConfig, Options } from "tsup";
 import { dependencies } from "./package.json";
 
-const commonConfig = {
-  define: {
-    "process.env.NODE_ENV": JSON.stringify("production"),
-  },
-  sourcemap: true,
-};
-
-export default defineConfig([
-  // Standard ESM
-  {
-    ...commonConfig,
+export default defineConfig((options) => {
+  const commonOptions: Partial<Options> = {
     entry: {
       index: "src/index.ts",
     },
-    target: "es2020",
-    format: ["esm"],
-    outDir: "./lib/esm/",
-    outExtension: () => ({ js: ".mjs" }),
-    dts: true,
-    clean: true,
-  },
-  // Support Webpack 4 by pointing `"module"` to a file with a `.js` extension
-  {
-    ...commonConfig,
-    entry: {
-      "index.legacy-esm": "src/index.ts",
+    sourcemap: true,
+    ...options,
+  };
+
+  return [
+    // Modern ESM
+    {
+      ...commonOptions,
+      format: ["esm"],
+      outExtension: () => ({ js: ".mjs" }),
+      dts: true,
+      clean: true,
+      outDir: "./lib/esm/",
     },
-    target: "es2017",
-    format: ["esm"],
-    outDir: "./lib/esm/",
-    outExtension: () => ({ js: ".js" }),
-  },
-  // Browser-ready ESM, minified
-  {
-    ...commonConfig,
-    entry: {
-      "index.browser": "src/index.ts",
+    // Support Webpack 4 by pointing `"module"` to a file with a `.js` extension
+    {
+      ...commonOptions,
+      entry: {
+        "index.legacy-esm": "src/index.ts",
+      },
+      format: ["esm"],
+      outExtension: () => ({ js: ".js" }),
+      target: "es2017",
+      outDir: "./lib/esm/",
     },
-    target: "es2020",
-    format: ["esm"],
-    outDir: "./lib/esm/",
-    outExtension: () => ({ js: ".js" }),
-    minify: true,
-    noExternal: Object.keys(dependencies),
-    skipNodeModulesBundle: false,
-    splitting: false,
-  },
-  // CJS, minified
-  {
-    ...commonConfig,
-    entry: {
-      "index.min": "src/index.ts",
+    // Browser-ready ESM, production + minified
+    {
+      ...commonOptions,
+      entry: {
+        "index.browser": "src/index.ts",
+      },
+      define: {
+        "process.env.NODE_ENV": JSON.stringify("production"),
+      },
+      format: ["esm"],
+      outExtension: () => ({ js: ".js" }),
+      minify: true,
+      noExternal: Object.keys(dependencies),
+      skipNodeModulesBundle: false,
+      splitting: false,
+      outDir: "./lib/esm/",
     },
-    target: "es2020",
-    format: ["cjs"],
-    outDir: "./lib/cjs/",
-    outExtension: () => ({ js: ".js" }),
-    minify: true,
-  },
-]);
+    // CJS
+    {
+      ...commonOptions,
+      format: ["cjs"],
+      outExtension: () => ({ js: ".cjs" }),
+      dts: true,
+      clean: true,
+      outDir: "./lib/cjs/",
+    },
+  ] as Options[];
+});
